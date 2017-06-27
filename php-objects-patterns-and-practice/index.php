a5be2a7919bdf9ecb63386ad5a2fd10071c332c6
ashucn <ashucn@qq.com>
2017-06-26 22:45:40 -0700

    init

---
+++ php-objects-patterns-and-practice/index.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+PHP 7.0.15
+
+~ A First Class
+~ A First Object (or Two)
+ */
+class ShopProduct
+{
+// class body
+}
+
+$product1 = new ShopProduct();
+$product2 = new ShopProduct();
+
+var_dump($product1);
+var_dump($product2);
+
+/*
+output: 
+object(ShopProduct)[1]
+object(ShopProduct)[2]
+ */
